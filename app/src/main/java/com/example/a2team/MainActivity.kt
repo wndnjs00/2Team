b88cdfc049ef717080238f876d3fcb5401ecdaf6
TodoList <juwonjeon852@gmail.com>
2024-02-27 10:49:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a2team/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.a2team
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.a2team.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.button1.setOnClickListener {
+            // Activityone으로 이동
+            val intent = Intent(this, Activityone::class.java)
+            startActivity(intent)
+        }
+
+
+        binding.button2.setOnClickListener {
+            // twoActivity 으로 이동
+            val intent = Intent(this, twoActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button3.setOnClickListener {
+            // threeActivity 으로 이동
+            val intent = Intent(this, threeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button4.setOnClickListener {
+            // FourActivity 으로 이동
+            val intent = Intent(this, FourActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a2team/threeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2team
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class threeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three)
+    }
+}
