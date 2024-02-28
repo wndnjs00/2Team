4285e2ef02eeeb85ba1f4d8cef43ed0b3bbecf5f
jeong-gyeongwoo <okbamtol@naver.com>
2024-02-28 17:01:23 +0900

    2조 정경우 페이지 커밋

--- app/src/main/java/com/example/a2team/threeActivity.kt
+++ app/src/main/java/com/example/a2team/threeActivity.kt
@@ -2,10 +2,18 @@ package com.example.a2team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class threeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_three)
+
+        val btn_back = findViewById<Button>(R.id.btn_back)
+
+        btn_back.setOnClickListener {
+            finish()
+        }
+
     }
 }
