7aa68c01a16fe6a7a79facf4d835c7684c72e665
AndroidJeong <okbamtol@gmail.com>
2024-02-29 18:04:24 +0900

    최종

--- app/src/main/java/com/example/a2team/Activityone.kt
+++ app/src/main/java/com/example/a2team/Activityone.kt
@@ -2,10 +2,18 @@ package com.example.a2team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Activityone : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activityone)
+
+        val btn_back = findViewById<Button>(R.id.btn_back)
+
+        btn_back.setOnClickListener {
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/a2team/twoActivity.kt
+++ app/src/main/java/com/example/a2team/twoActivity.kt
@@ -2,10 +2,20 @@ package com.example.a2team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class twoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two)
+
+        val btn_back = findViewById<Button>(R.id.btn_back)
+
+        btn_back.setOnClickListener {
+            finish()
+        }
+
+
+
     }
 }
